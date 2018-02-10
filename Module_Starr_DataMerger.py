9b84439faa9938c100c959126e7b6c8e30a83ff4
Born2Ride <30387055+ccirelli2@users.noreply.github.com>
2018-02-10 08:06:50 -0500

    Updates
    Finalized all code an notes.
    
    What remains is to create a pipeline function for the get_match() function and data merge portion of this project.

--- Module_Starr_DataMerger.py
+++ Module_Starr_DataMerger.py
@@ -13,7 +13,11 @@ def hello_word():
 def get_nanValues(Dataframe):
     '''The purpose of this function is to count nan values of a dataframe
     Input  = Dataframe.  Iterates over each column.
-    Output = Dictionary with count of none values per column. '''
+    Output = Dictionary with count of none values per column. 
+    
+    date:    02.10.2018
+    author:  Chris Cirelli
+    '''
 
     # Dict to catch values per column. 
     Dict = {}
@@ -43,23 +47,34 @@ def get_nanValues(Dataframe):
 
 def get_company_name(Dataframe, Position):
 
-    '''The purpose of this code is to clean the comapny name 
-    1.) Remove comma
-    2.) Split on space
-    3.) Create a list for the first and second words of the company name. '''
-
-    # Get List of Company names
+    '''The purpose of this code is to obtain either the first or second company name from a either the SF or CIQ datasets. 
+    
+    Input  = a.) the target dataframe, b.) either 'First' or 'Second' position to indicate which part of the name to generate. 
+    Output = A list of either the first or second name w/ ',' and '.' removed. 
+    
+    date:   02.10.2018
+    author: Chris Cirelli
+    '''
+    
+    # Obtain the column where the Company Names are located. 
     List_company_name = Dataframe['Company Name']
      
-    # Clean Lists - Remove Commas / Split on spaces
+    # Clean Names
     
+    # Convert all names to string 
     CIQ_string = [str(word) for word in List_company_name]
-    CIQ_replace_comma = [word.replace(',', '') for word in CIQ_string] 
+    # Replace comma with space
+    CIQ_replace_comma = [word.replace(',', '') for word in CIQ_string]
+    # Split on spaces
     CIQ_split_on_space = [word.split(' ' ) for word in List_company_name]    
     
+    
+    # Create a list to capture the first or second names generated from the below function
     List_name = []
     
+    # Iterate over the CIQ dataframe. 
     for x in CIQ_split_on_space:
+        
         # if Position == First, append the first name to List_name
         if Position == 'First':
             List_name.append(x[0])
@@ -74,25 +89,44 @@ def get_company_name(Dataframe, Position):
                 x.append('placeholder')
                 List_name.append(x[1])
     
+    # Replace any remaining commas with a space.  
     List_final = [x.replace(',','') for x in List_name]
     
-    
+    # Return the final list. 
     return List_final
 
 
-def clean_zip_code(String, Dataframe):
+def clean_zip_Code(Dataframe_str, Dataframe):
+    '''The purpose of this code is to generate a zipCode with a single format for comparison purposes.  
+    Input  = a.) a string version of the target dataframe. b.) The target dataframe. 
+    Output = a list of zipCodes for each record with a standardized format. 
     
+    date:   02.10.2018
+    author: Chris Cirelli
+    '''
+    
+    # Identify the target dataframe. 
     if String == 'DAT_SF':
         
+        # Obtain the zipCode column of the SF dataframe 
         DAT_SF_ZIP = Dataframe['Billing Zip/Postal Code']
+        
+        # Create a list to capture the clean zipCode values. 
         DAT_SF_Zip_Code_Clean = []
-    
+        
+        # Iterate over the zipCode column. 
         for x in DAT_SF_ZIP:
+            # Conver int to string. 
             x_string = str(x)
+            # Split any zipCodes on '-'
             x_split = x_string.split('-')
+            # Take the part of the zipCode in position one. 
             DAT_SF_Zip_Code_Clean.append((x_split[0]))
-    
+            
+        # Return a list of the zipCodes n the standardized format.  
         return DAT_SF_Zip_Code_Clean
+    
+    # Note:  the below code repeats the same proces, but for the CIQ dataframe. 
     
     elif String == 'DAT_CIQ':
         
@@ -108,11 +142,26 @@ def clean_zip_code(String, Dataframe):
     
 
 def get_match_v2():
-
-    CIQ = [x for x in DAT_head.itertuples()]
+    '''The purpose of this code is to match records from the SF and CIQ dataframes. 
+    Input  = The CIQ and SF Dataframes.  Requires that these dataframes were pre-cleaned by the codes included in the
+             Module_Starr_Datamerge file. 
+    Output = The DAT_CIQ Dataframe with the matching values appended to each row. 
+    
+    Date:    02.10.2018
+    author:  Chris Cirelli
+    '''
+    
+    # Create a tuple for each row in the dataframe. 
+    CIQ = [x for x in CIQ_head.itertuples()]         # Rever back to DAT CIQ when finished testing. 
     SF = [x for x in DAT_SF.itertuples()]
-
+    
+    # Loop over each row of the CIQ Dataframe. 
     for row_CIQ in CIQ:
+        
+        # Get the index value for the target CIQ row.  Use this at end of code. 
+        row_CIQ_index_value = row_CIQ.index
+        
+        # Loop over each row of the SF Dataframe. 
         for row_SF in SF:
             if row_CIQ[8] in row_SF[8]:
             
@@ -135,6 +184,7 @@ def get_match_v2():
                         # Create a new SF tupple object from the SF limited dataframe. 
                         SF_3 = [x for x in SF_limited_firstName.itertuples()]
                         
+                        
                         # Iterate over new SF dataframe
                         for row_SF3 in SF_3:
                             
@@ -147,16 +197,26 @@ def get_match_v2():
                                 SF_Final = SF_limited_firstName[SF_limit]
                         
                                 return SF_Final
-                            
+                        
 
             
 def write_to_excel(dataframe, filename):
+    '''The purpose of this code is to write a pandas dataframe to excel. 
+    
+    Input  = a.) Target dataframe, b.) String representing the resulting file name. 
+    Output = an Excel spreadsheet with the data from the target dataframe. 
+    '''
+    # Ensure that pandas is imported. 
     import pandas as pd
+    # Choose an Excel writer. 
     writer = pd.ExcelWriter(filename+'.xlsx')
+    # Transfer the dataframe to Excel and name the sheet 'Data'
     dataframe.to_excel(writer, sheet_name = 'Data')
+    # Save the Excel file. 
     writer.save()
 
-
+    # return none as this function writes directly to the current directory. 
+    return None
 
 
 
